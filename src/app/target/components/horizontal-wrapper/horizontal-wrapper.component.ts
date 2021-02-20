aa4e2613568b1f43f9a2b2ffe9f0f2945076632a
Ardenexal <alex@halaxy.com>
2021-02-20 23:17:40 +1100

    Fixed test

--- src/app/target/components/horizontal-wrapper/horizontal-wrapper.component.ts
+++ src/app/target/components/horizontal-wrapper/horizontal-wrapper.component.ts
@@ -4,7 +4,6 @@ import {FieldWrapper} from "@ngx-formly/core";
 @Component({
   selector: 'hx-horizontal-wrapper',
   templateUrl: './horizontal-wrapper.component.html',
-  styleUrls: ['./horizontal-wrapper.component.scss']
 })
 export class HorizontalWrapperComponent extends FieldWrapper {
 }
